c91a3c6a33c4c45aac834b5a884f9971c0e02f93
webdiller <eugenefromrus@gmail.com>
2026-03-24 23:40:04 +1000

    upd: upd app

--- app/affiliate/cases/[slug]/page.tsx
+++ app/affiliate/cases/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+
 import { notFound } from "next/navigation"
 import { AffiliateCaseDetail } from "@/components/affiliate/affiliate-case-detail"
 import type { Partner, PartnerCategory } from "@/components/cases"
